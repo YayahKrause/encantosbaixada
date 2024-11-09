f5856a9730cb091a4e2b965fb6846e9ed26fad5e
Nayuki <62123949+YayahKrause@users.noreply.github.com>
2024-11-08 23:30:26 -0300

    Add files via upload

---
+++ index.html/forms.sql
@@ -0,0 +1,7 @@
+CREATE TABLE formulario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    sobrenome VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    recomendacoes TEXT
+);
